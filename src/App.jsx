8d3e8ee93c92ff7011a6f20b62a17c77d18442e3
Mykhailo Solonin <msolonin@anydesk.com>
2024-09-29 17:22:58 +0200

    Initial commit 2

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import {toast, Toaster} from "react-hot-toast";
+import {useState} from 'react'
+import './App.css'
+import SearchBar from "./components/SearchBar/SearchBar.jsx";
+import ImageGallery from "./components/ImageGallery/ImageGallery.jsx";
+import {fetchPhotos} from "./js/unsplash-api.js";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn.jsx";
+import ImageModal from "./components/ImageModal/ImageModal.jsx";
+import Loader from "./components/Loader/Loader.jsx";
+
+function App() {
+    const [photos, setPhotos] = useState([]);
+    const [page, setPage] = useState(1);
+    const [queryString, setQueryString] = useState('');
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const onSubmit = async (submitValue) => {
+        try {
+            setLoading(true);
+            const newPhotos = await fetchPhotos(submitValue, 1, 10); // Fetch new photos
+            setPhotos(newPhotos);
+            setQueryString(submitValue);
+        } catch (error) {
+            toast.error("Error fetching photos: " + error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const onLoadMore = async () => {
+        try {
+            setLoading(true);
+            const newPhotos = await fetchPhotos(queryString, page + 1, 10); // Fetch new photos
+            setPhotos([...photos, ...newPhotos]); // Set state with fetched photos
+            setPage(page + 1); // Increase page number
+        } catch (error) {
+            toast.error("Error fetching photos: " + error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const openModal = (photo) => {
+        setSelectedImage(photo);
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setSelectedImage(null);
+        setModalOpen(false);
+    };
+
+    return (
+        <>
+            <SearchBar onSubmit={onSubmit} />
+            {photos.length > 0 ? (
+                <ImageGallery photos={photos} openModal={openModal} />
+            ) : (
+                <p>Nothing to display.</p>
+            )}
+            {loading && <Loader />}
+            {photos.length > 0 && <LoadMoreBtn onLoadMore={onLoadMore} />}
+            <ImageModal isOpen={isModalOpen} onRequestClose={closeModal} image={selectedImage} />
+        </>
+    );
+}
+
+export default App;
